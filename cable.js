116f052e400dfca299824f53ca4601d63c60ba77
july183 <julysmj183@gmail.co>
2022-02-28 15:20:49 +0900

    ooo

--- cable.js
+++ cable.js
@@ -14,7 +14,14 @@ cable.get('/cable',function(req,res){
     res.send(' cable 의  두번째 페이지 ')
 });
 
+cable.get('/preinterview',function(req, res){
+    res.send('사전면접');
 
+});
+cable.get('/contact',function(req, res){
+    res.send('면접제안');
+
+});
 
 
 
